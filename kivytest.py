ef7f2b11e4fafd332ad5c1853bd2dceabd8cc40a
Alberto Perugini <albertoperugini.ap@gmail.com>
2021-04-03 16:10:37 +0200

    kivy seems like better way to go, first tests

---
+++ kivytest.py
@@ -0,0 +1,37 @@
+import kivy
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
+
+Builder.load_file('layouts.kv')
+
+
+class MainPage(Screen):
+
+    def press(self):
+        pass
+
+
+class ShotsPage(Screen):
+    pass
+
+
+class DrinksPage(Screen):
+    pass
+
+
+class MyApp(App):
+    n_cols = NumericProperty(2)
+
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainPage(name='main_page'))
+        sm.add_widget(ShotsPage(name='shots_page'))
+        sm.add_widget(DrinksPage(name='drinks_page'))
+
+        return sm
+
+
+if __name__ == '__main__':
+    MyApp().run()
--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import tkinter as tk
+from MainPage import *
 
-window = tk.Tk()
+root = tk.Tk()
 
-btn_shots = tk.Button(
-    text="SHOTS",
-    height=10,
-    master=window
-)
-
-btn_drinks = tk.Button(
-    text="DRINKS",
-    height=10,
-    master=window
-)
+app = MainPage(master=root)
+app.mainloop()
 
 
-def handle_btn_shots(event):
-    pass
+# window = tk.Tk()
+
+# btn_shots = tk.Button(
+#     text="SHOTS",
+#     height=10,
+#     master=window
+# )
+
+# btn_drinks = tk.Button(
+#     text="DRINKS",
+#     height=10,
+#     master=window
+# )
 
 
-def handle_btn_drinks(event):
-    pass
+# def handle_btn_shots(event):
+#     pass
 
 
-btn_shots.bind("<Button-1>", handle_btn_shots)
-btn_drinks.bind("<Button-1>", handle_btn_drinks)
+# def handle_btn_drinks(event):
+#     pass
 
-btn_shots.pack(fill=tk.X)
-btn_drinks.pack(fill=tk.X)
-window.mainloop()
+
+# btn_shots.bind("<Button-1>", handle_btn_shots)
+# btn_drinks.bind("<Button-1>", handle_btn_drinks)
+
+# btn_shots.pack(fill=tk.X)
+# btn_drinks.pack(fill=tk.X)
+# window.mainloop()
